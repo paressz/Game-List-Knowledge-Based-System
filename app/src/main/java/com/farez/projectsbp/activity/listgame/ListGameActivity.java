6bb65000f2d7e22b879421f9022c36b2346d8235
paress <farez0830@gmail.com>
2023-12-07 07:47:56 +0700

    UX update,  reformat code

--- app/src/main/java/com/farez/projectsbp/activity/listgame/ListGameActivity.java
+++ app/src/main/java/com/farez/projectsbp/activity/listgame/ListGameActivity.java
@@ -1,14 +1,14 @@
 package com.farez.projectsbp.activity.listgame;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.farez.projectsbp.data.model.Game;
 import com.farez.projectsbp.databinding.ActivityListGameBinding;
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class ListGameActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+public class ListGameActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, View.OnClickListener {
     RecyclerView rv;
     ListGameAdapter listGameAdapter;
     ActivityListGameBinding binding;
@@ -41,6 +41,7 @@ public class ListGameActivity extends AppCompatActivity implements CompoundButto
         getGamesFromDatabase(showAllGames);
         setupRecyclerView();
         binding.switch1.setOnCheckedChangeListener(this);
+        binding.imageView.setOnClickListener(this);
 
     }
 
@@ -62,18 +63,17 @@ public class ListGameActivity extends AppCompatActivity implements CompoundButto
                     listGameAdapter.setGameList(filteredList);
                     if (filteredList.isEmpty()) binding.tvNoGame.setVisibility(View.VISIBLE);
                     else binding.tvNoGame.setVisibility(View.GONE);
-                }
-                else {
+                } else {
                     binding.tvNoGame.setVisibility(View.VISIBLE);
                     Toast.makeText(this, "ERROR : list game bernilai null", Toast.LENGTH_SHORT).show();
                 }
-            }
-            else {
+            } else {
                 gameList = games;
                 listGameAdapter.setGameList(gameList);
             }
         });
     }
+
     //Fungsi buat handle kalo switch 18+ diganti
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean value) {
@@ -89,36 +89,43 @@ public class ListGameActivity extends AppCompatActivity implements CompoundButto
             }
         }
     }
-// method buat handle search berdasarkan
+
+    // method buat handle search berdasarkan
 // data cpu, ram, hdd, dan vga
 // yang diinput dari spekInputActivity
     void handleSearch() {
         Serializable spekData = getIntent().getSerializableExtra(KeyUtil.KEY_INTENT_SPEK);
         if (spekData != null) {
             keywordSearch = (HashMap<String, String>) spekData;
-        } else  {
+        } else {
             Toast.makeText(this, "DATA SPEK TIDAK ADA", Toast.LENGTH_SHORT).show();
         }
         gameList = gameList
                 .stream()
                 .filter(
-                game ->
-                        game.getCpu().toUpperCase().trim().contains(keywordSearch.get(KeyUtil.KEY_CPU).toUpperCase().trim())
-                                &&
-                        game.getRam() <= Integer.parseInt(keywordSearch.get(KeyUtil.KEY_RAM))
-                                &&
-                        game.getHdd() <= Integer.parseInt(keywordSearch.get(KeyUtil.KEY_HDD))
-                                ||
-                        game.getVga().toUpperCase().trim().contains(keywordSearch.get(KeyUtil.KEY_VGA).toUpperCase().trim())
+                        game ->
+                                game.getCpu().toUpperCase().trim().contains(keywordSearch.get(KeyUtil.KEY_CPU).toUpperCase().trim())
+                                        &&
+                                        game.getRam() <= Integer.parseInt(keywordSearch.get(KeyUtil.KEY_RAM))
+                                        &&
+                                        game.getHdd() <= Integer.parseInt(keywordSearch.get(KeyUtil.KEY_HDD))
+                                        ||
+                                        game.getVga().toUpperCase().trim().contains(keywordSearch.get(KeyUtil.KEY_VGA).toUpperCase().trim())
                 )
                 .collect(Collectors.toList());
     }
+
     void checkIsListEmpty() {
         if (filteredList.isEmpty()) {
             binding.tvNoGame.setVisibility(View.VISIBLE);
-        }
-        else {
+        } else {
             binding.tvNoGame.setVisibility(View.GONE);
         }
     }
+
+    @Override
+    public void onClick(View view) {
+        onBackPressed();
+    }
+
 }
