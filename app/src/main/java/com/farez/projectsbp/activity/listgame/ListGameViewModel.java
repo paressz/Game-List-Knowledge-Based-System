d0aa1041a5c38c0e0c53fa05884433cf161f9144
paress <farez0830@gmail.com>
2023-12-08 16:30:16 +0700

    delete unused imports

--- app/src/main/java/com/farez/projectsbp/activity/listgame/ListGameViewModel.java
+++ app/src/main/java/com/farez/projectsbp/activity/listgame/ListGameViewModel.java
@@ -5,15 +5,11 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-
 import com.farez.projectsbp.data.database.GameDao;
 import com.farez.projectsbp.data.database.GameDatabase;
 import com.farez.projectsbp.data.model.Game;
 
 import java.util.List;
-import java.util.concurrent.Executors;
 
 public class ListGameViewModel extends AndroidViewModel {
 
